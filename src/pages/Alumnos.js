5eb89f94ee59cae8e4e04f11f5ac342b06a9f4af
Pedro Mendoza <pmendoza@caracolknits.com>
2022-06-07 16:07:56 -0600

    creacion del proyecto - diseno

---
+++ src/pages/Alumnos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Alumnos = () => {
+  return (
+    <div>Alumnos</div>
+  )
+}
---
+++ src/pages/Maestros.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Maestros = () => {
+  return (
+    <div>Maestros</div>
+  )
+}
